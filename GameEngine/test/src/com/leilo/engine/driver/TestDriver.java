5e6a88b0567867111ddfa8849f9e63147b690f07
Patrick Thomas <pet.pthomas@gmail.com>
2014-11-08 11:23:12 -0500

    Adding test utilities

--- GameEngine/test/src/com/leilo/engine/driver/TestDriver.java
+++ GameEngine/test/src/com/leilo/engine/driver/TestDriver.java
@@ -1,5 +1,6 @@
 package src.com.leilo.engine.driver;
 
+import com.leilo.engine.characters.PlayerCharacter;
 import com.leilo.engine.items.Consumable;
 import com.leilo.engine.items.Equipment;
 import com.leilo.engine.items.Inventory;
@@ -28,11 +29,74 @@ public class TestDriver {
 
     public static void main(String[] args) {
         WidthLimitedOutputStream outputStream = new WidthLimitedOutputStream(System.out, 80);
-        testPrintUtils(outputStream);
+        //testPlayerCharacter(outputStream);
+        //testPrintUtils(outputStream);
+
+    }
+
+    public static void testPlayerCharacter(WidthLimitedOutputStream outputStream) {
+        Room testRoom = new Room("testID", "Test Room", ".", null);
+        PlayerCharacter testPlayer = new PlayerCharacter("Player", 100, testRoom);
+        List<Item> itemList = new ArrayList<Item>();
+        itemList.add(new Consumable("ID_2", "Beer", "Description"));
+        itemList.add(new Equipment("ID_6", "Sword", "Description", 0));
+        itemList.add(new Equipment("ID_7", "Axe", "Description", 0));
+        itemList.add(new Equipment("ID_8", "Mace", "Description", 0));
+        itemList.add(new Equipment("ID_9", "Helmet", "Description", 1));
+        itemList.add(new Equipment("ID_9", "Helmet", "Description", 1));
+        itemList.add(new Equipment("ID_10", "Hat", "Description", 1));
+        itemList.add(new Equipment("ID_11", "Mail Cuirass", "Description", 2));
+        itemList.add(new Equipment("ID_11", "Mail Cuirass", "Description", 2));
+        itemList.add(new Equipment("ID_12", "Pants", "Description", 4));
+        itemList.add(new Equipment("ID_13", "Boots", "Description", 5));
+        testPlayer.addItemsToPlayerInventory(itemList);
+        outputStream.println("Initial Inventory & Equipment: ");
+        PrintUtils.getInstance().printInventory(testPlayer.getPlayerInventory(), outputStream);
+        outputStream.println();
+        PrintUtils.getInstance().printEquipmentMap(testPlayer.getPlayerEquipmentMap(), outputStream);
+        outputStream.println();
+        testPlayer.equipItemToPlayer("ID_8");
+        outputStream.println("Equipped Mace: ");
+        PrintUtils.getInstance().printInventory(testPlayer.getPlayerInventory(), outputStream);
+        outputStream.println();
+        PrintUtils.getInstance().printEquipmentMap(testPlayer.getPlayerEquipmentMap(), outputStream);
+        outputStream.println();
+        testPlayer.equipItemToPlayer("ID_9");
+        outputStream.println("Equipped Helmet: ");
+        PrintUtils.getInstance().printInventory(testPlayer.getPlayerInventory(), outputStream);
+        outputStream.println();
+        PrintUtils.getInstance().printEquipmentMap(testPlayer.getPlayerEquipmentMap(), outputStream);
+        outputStream.println();
+        testPlayer.equipItemToPlayer("ID_10");
+        outputStream.println("Equipped Hat: ");
+        PrintUtils.getInstance().printInventory(testPlayer.getPlayerInventory(), outputStream);
+        outputStream.println();
+        PrintUtils.getInstance().printEquipmentMap(testPlayer.getPlayerEquipmentMap(), outputStream);
+        outputStream.println();
+        testPlayer.equipItemToPlayer("ID_11");
+        outputStream.println("Equipped Cuirass: ");
+        PrintUtils.getInstance().printInventory(testPlayer.getPlayerInventory(), outputStream);
+        outputStream.println();
+        PrintUtils.getInstance().printEquipmentMap(testPlayer.getPlayerEquipmentMap(), outputStream);
+        outputStream.println();
+        testPlayer.equipItemToPlayer("ID_12");
+        outputStream.println("Equipped Pants: ");
+        PrintUtils.getInstance().printInventory(testPlayer.getPlayerInventory(), outputStream);
+        outputStream.println();
+        PrintUtils.getInstance().printEquipmentMap(testPlayer.getPlayerEquipmentMap(), outputStream);
+        outputStream.println();
+        outputStream.println("Done.");
     }
 
     private static void testPrintUtils(WidthLimitedOutputStream outputStream) {
         //Test Printing of Current Location
+        //printCurrentRoomTest(outputStream);
+        //outputStream.println("*******************************************************");
+        //Test Printing of Inventory
+        printInventoryTest(outputStream);
+    }
+
+    private static boolean printCurrentRoomTest(WidthLimitedOutputStream outputStream) {
         outputStream.println("Testing Printing of Current Location");
         try {
             GameWorld testWorld = new GameWorld();
@@ -44,22 +108,25 @@ public class TestDriver {
                 testWorld.setCurrentRoom(testGameMap.get("testroom"+i));
                 PrintUtils.getInstance().printCurrentRoom(testWorld.getCurrentRoom(), testWorld, outputStream);
                 outputStream.println();
+                outputStream.println();
             }
         } catch (Exception e) {
             outputStream.println("Error occurred during Current Location Print Testing: " + e.getLocalizedMessage());
+            return false;
         }
+        return true;
+    }
 
+    private static boolean printInventoryTest(WidthLimitedOutputStream outputStream) {
+        outputStream.println("Empty Inventory Test");
+        Inventory emptyInventory = new Inventory(20);
+        PrintUtils.getInstance().printInventory(emptyInventory, outputStream);
+        outputStream.println("*******************************************************");
         outputStream.println();
-        outputStream.println();
-        outputStream.println("*****************************************");
-        outputStream.println();
-        outputStream.println();
-
-        //Test Printing of Inventory
         outputStream.println("Testing Printing of Inventory");
         Inventory testInventory = new Inventory(50);
         List<Item> itemList = new ArrayList<Item>();
-        itemList.add(new Consumable("ID_1", "A Consumable Item", "Description"));
+        itemList.add(new Consumable("ID_1", "A Consumable Item Item Item Item Item ", "Description"));
         itemList.add(new Consumable("ID_2", "Beer", "Description"));
         itemList.add(new Consumable("ID_2", "Beer", "Description"));
         itemList.add(new Consumable("ID_3", "Consumable Item", "Description"));
@@ -79,5 +146,6 @@ public class TestDriver {
         itemList.add(new Equipment("ID_13", "Boots", "Description", 5));
         testInventory.addItemsToInventory(itemList);
         PrintUtils.getInstance().printInventory(testInventory, outputStream);
+        return true;
     }
 }
---
+++ GameEngine/test/testGame/driver/TestGameDriver.java
@@ -0,0 +1,118 @@
+package testGame.driver;
+
+import com.leilo.engine.characters.PlayerCharacter;
+import com.leilo.engine.items.Consumable;
+import com.leilo.engine.items.Equipment;
+import com.leilo.engine.items.Item;
+import com.leilo.engine.room.Room;
+import com.leilo.engine.utils.*;
+import com.leilo.engine.world.GameWorld;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * Main game driver class
+ */
+public class TestGameDriver {
+    private String m_testFile = "./test/testGame/testSaves/testGame.dat";
+    private File m_mapPropertiesFile = new File("./test/testGame/properties/TestGameMap.properties");
+    private File m_itemPropertiesFile = new File("./test/testGame/properties/TestGameItems.properties");
+    //todo add this nonsense private File m_mobPropertiesFile = new File("./test/resources/properties/TestMobs.properties");
+    private Map<String, Room> m_gameMap;
+    private GameWorld m_game;
+    private PlayerCharacter m_player;
+
+    // Output stream for writing to console
+    transient private WidthLimitedOutputStream m_output;
+
+    public TestGameDriver() {
+        m_output = new WidthLimitedOutputStream(System.out, 80);
+        try {
+            init();
+        } catch (Exception e) {
+            m_output.println("Error initializing the game world");
+            return;
+        }
+    }
+
+    public static void main(String[] args) {
+        new TestGameDriver().play();
+    }
+
+    public void init() throws Exception {
+        Properties roomProperties = PropertyUtils.getInstance().loadPropertyFile(m_mapPropertiesFile);
+        Properties itemProperties = PropertyUtils.getInstance().loadPropertyFile(m_itemPropertiesFile);
+        //todo add this nonsense Properties mobProperties = PropertyUtils.getInstance().loadPropertyFile(m_mobPropertiesFile);
+        m_gameMap = MapCreator.getInstance().createGameMap(roomProperties, null, itemProperties, m_output);
+
+        //Create the Player
+        m_player = new PlayerCharacter("TestPlayer", 100, m_gameMap.get("testroom1"));
+        m_player.addItemsToPlayerInventory(createItemList());
+
+        //Create the game World
+        m_game = new GameWorld();
+        m_game.addRoomsToGame(m_gameMap);
+        //m_game.addPlayerCharacter(m_player);
+        m_game.setCurrentRoom(m_gameMap.get("testroom1"));
+
+    }
+
+    private void saveGame(File gameFile) throws Exception{
+        // Create a file to write game system
+        FileOutputStream out = new FileOutputStream(gameFile, false);
+        // Create an object output stream, linked to out
+        ObjectOutputStream objectOut = new ObjectOutputStream(out);
+        // Write game system to object store
+        objectOut.writeObject(m_game);
+        // Close object output stream
+        objectOut.close();
+        m_output.println("Game data created at " + m_testFile);
+    }
+
+    public void play() {
+        String command = null;
+        // Create a BufferedReader
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while(m_game.isGameActive())
+        {
+            // Show location
+            PrintUtils.getInstance().printCurrentRoom(m_game.getCurrentRoom(), m_game, m_output);
+            m_output.println();
+            try {
+                // Get user input
+                command = reader.readLine().trim();
+                m_game = CommandParser.getInstance().parseCommand(m_game, command, m_output);
+                m_output.println();
+            } catch (Exception e)  {
+                m_output.println("ERROR");
+            }
+        }
+    }
+
+    private List<Item> createItemList() {
+        List<Item> itemList = new ArrayList<Item>();
+        itemList.add(new Consumable("ID_1", "A Consumable Item Item Item Item Item ", "Description"));
+        itemList.add(new Consumable("ID_2", "Beer", "Description"));
+        itemList.add(new Consumable("ID_2", "Beer", "Description"));
+        itemList.add(new Consumable("ID_3", "Consumable Item", "Description"));
+        itemList.add(new Consumable("ID_3", "Consumable Item", "Description"));
+        itemList.add(new Consumable("ID_3", "Consumable Item", "Description"));
+        itemList.add(new Consumable("ID_4", "Derp", "Description"));
+        itemList.add(new Consumable("ID_5", "Barnacles", "Description"));
+        itemList.add(new Equipment("ID_6", "Sword", "Description", 0));
+        itemList.add(new Equipment("ID_7", "Axe", "Description", 0));
+        itemList.add(new Equipment("ID_8", "Mace", "Description", 0));
+        itemList.add(new Equipment("ID_9", "Helmet", "Description", 1));
+        itemList.add(new Equipment("ID_9", "Helmet", "Description", 1));
+        itemList.add(new Equipment("ID_10", "Hat", "Description", 1));
+        itemList.add(new Equipment("ID_11", "Mail Cuirass", "Description", 2));
+        itemList.add(new Equipment("ID_11", "Mail Cuirass", "Description", 2));
+        itemList.add(new Equipment("ID_12", "Pants", "Description", 4));
+        itemList.add(new Equipment("ID_13", "Boots", "Description", 5));
+        return itemList;
+    }
+}
